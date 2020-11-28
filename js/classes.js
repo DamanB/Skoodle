6706a3614930240f81a9d11567d421a963043776
akarjun8147 <kaushik.81@hotmail.com>
2020-11-28 14:14:39 -0500

    skeleton of classes made

--- js/classes.js
+++ js/classes.js
@@ -12,11 +12,13 @@ class Admin
 
 class Secretary
 {
-    constructor(name, username, password,type)
+    constructor(name, username, password,StudentList,ClassList, type)
     {
         this.name = name;
         this.username = username;
         this.password = password;
+        this.StudentList = new Array(Student); 
+        this.ClassList = new Array(Classroom); 
         this.type = "SECRETARY"; 
     }
 
@@ -62,13 +64,55 @@ class Parent
 
 }
 
+class Classroom
+{
+    constructor(name,teacher, timeinterval,StudentList)
+    {
+        this.name = name; 
+        this.teacher = new Teacher();
+        this.timeinterval = timeinterval;
+        this.StudentList = new Array(Student);
+
+    }
+
+
+}
+
+//this is just a single entry within the major attendance sheet
+class AttendanceEntry
+{
+    constructor(StudentName,className, date, studentStatus)
+    {
+        this.className = className; 
+        this.StudentName = StudentName; 
+        this.date = new Date();
+        this.studentStatus = false;  
+
+    }
+    
+
+}
+
+//attendance which teacher fills out for their classes 
+class ClassAttendance
+{
+    constructor(ListAttendances)
+    {
+        this.ListAttendances = new Array(AttendanceEntry); 
+
+    }
+
+}
+
 
 class Student 
 {
-    constructor(name,tempid) 
+    constructor(name,Stdid,classes, attendanceList) 
     {
       this.name = name;
-      this.tempid = tempid; 
+      this.Stdid = Stdid;
+      this.classes = new Array(Classroom); 
+      this.attendanceList = new Array(Attendance)
     }
 }
 
