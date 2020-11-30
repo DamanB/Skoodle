7be4197d43a6be762b5e0620eb3d845b39d09622
akarjun8147 <kaushik.81@hotmail.com>
2020-11-30 14:19:43 -0500

    adding data

--- js/data.js
+++ js/data.js
@@ -1,1 +1,19 @@
-var admin = new Admin("admin404", "notfound");
+var admin_1 = new Admin("Adam", "1234"); 
+
+var sec_1 = new Secretary("Susan", "sec1", "123"); 
+
+var teach_1 = new Teacher("John", "teach1", "12345");
+
+var teach_2 = new Teacher("Johnathan", "teach2", "123456"); 
+
+var teach_3 = new Teacher("Johnny", "teach3", "123456");
+
+
+
+function InitData()
+{
+       
+
+
+}
+
